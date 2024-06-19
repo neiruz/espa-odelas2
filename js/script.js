bce5d02cf81a01f8dc71cb161ba19f4c048c70b8
critine <166624314+neiruz@users.noreply.github.com>
2024-06-19 07:27:16 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,4 +4,17 @@ let navlist = document.querySelector('.navlist');
 menu.onclick = () => { 
     menu.classList.toggle('bx-x');
     navlist.classList.toggle('open');
-}
+};
+
+const sr = ScrollReveal({
+    distance:'65px',
+    duration: 2600,
+    delay:450,
+    reset: true
+
+});
+
+
+sr.reveal('.hero-text', {delay:200,origin:'top'});
+sr.reveal('.hero-img', {delay:450,origin:'top'});
+sr.reveal('.icons', {delay:500,origin:'left'});
